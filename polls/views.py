1df9381a26787539136220196f28c567cabbbb81
Jiez Tee <jieztee@gmail.com>
2020-12-12 10:17:44 +0800

    Generic View

--- polls/views.py
+++ polls/views.py
@@ -1,22 +1,27 @@
 from django.shortcuts import render, get_object_or_404
-
-# Create your views here.
 from django.http import HttpResponse, response, Http404, HttpResponseRedirect
 from django.urls import reverse
+from django.views import generic
+
 from .models import Choice, Question
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_qeustion_list': latest_question_list}
-    return render(request,'polls/index.html',context)
+# Create your views here.
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
-def results(request, question_id):
-    response = "You 're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    def get_queryset(self):
+        """"Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
